f0d9fa8aebca443065a45a66f9923d7f99c031e1
--get <nutpeg@gmail.com>
2015-02-09 16:41:37 +0000

    Fix Oregon Coast Tour page. Add route

--- meadowlark.js
+++ meadowlark.js
@@ -37,6 +37,11 @@ app.get('/tours/hood-river', function (req, res) {
   res.render('tours/hood-river');
 });
 
+// Oregon Coast page
+app.get('/tours/oregon-coast', function (req, res) {
+  res.render('tours/oregon-coast');
+});
+
 // Request Group Rate page
 app.get('/tours/request-group-rate', function (req, res) {
   res.render('tours/request-group-rate');
